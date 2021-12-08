732dcaf1fbadf6673054a79e3f3420c9e2435fd5
AlexeyYukhin <alex.yukhin@gmail.com>
2021-12-08 22:04:05 +0200

    singleton pattern template added

---
+++ Singleton.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+/*
+    Meyer's threadsafe Singleton pattern
+    Usage:
+    class Foo : public Singleton<Foo> {
+        friend class Singleton;
+        private:
+            Foo();
+    };
+*/
+
+template<typename T>
+struct Singleton{
+    Singleton(const Singleton&) = delete;
+    Singleton(Singleton &&) = delete;
+    Singleton& operator=(const Singleton &) = delete;
+    Singleton & operator=(Singleton &&) = delete;
+
+    static T& instance(){
+        static T instance;
+        return instance;
+    }
+protected:
+    Singleton() = default;
+};
